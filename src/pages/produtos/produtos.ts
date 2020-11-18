f18dc99c1a99f24a5a0515030fcd4851dcc8b52d
Vanderson Nogueira <vanderson.01@hotmail.com>
2020-11-18 14:19:06 -0300

    URL dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../dominio/produto.dto';
 import { ProdutoService } from '../../servicos/dominio/produto.service';
 
@@ -31,10 +32,32 @@ export class ProdutosPage {
     this.service.findByCategoria(categoria_id).subscribe(
       response => {
         this.produtos = response["content"];
+        this.imageExists();
       }, error => {
 
       }
     );
+
+  }
+
+  /**
+   *  Envia uma requisição para o bucket para saber se a imagem existe
+   * 
+   * **/
+  imageExists() {
+      
+     
+    this.produtos.forEach(produto => {
+      // verificando se o produto existe
+      console.log( produto);
+      this.service.pegarImagemPequena(produto.id).subscribe(
+        // se existir seta a url do produto
+        response => {
+          produto.imageURL = `${API_CONFIG.bucketBaseURL}/prod${produto.id}-small.jpg`;
+        }
+        , error => { });
+    });
+
 
   }
 }
--- src/servicos/dominio/produto.service.ts
+++ src/servicos/dominio/produto.service.ts
@@ -9,19 +9,30 @@ import { ProdutoDTO } from "../../dominio/produto.dto";
 
 //para ser intetável
 @Injectable()
-export class ProdutoService{
+export class ProdutoService {
 
 
-   constructor(public http: HttpClient){
+   constructor(public http: HttpClient) {
 
    }
 
    /**
     *  Realiza a busca pagina de produtos
     * ***/
-   findByCategoria(categoria_id : string) : Observable<ProdutoDTO[]>{
-         
-    return this.http
-            .get<ProdutoDTO[]>(`${API_CONFIG.baseUrl}/produtos/page?categorias=${categoria_id}`);
+   findByCategoria(categoria_id: string): Observable<ProdutoDTO[]> {
+
+      return this.http
+         .get<ProdutoDTO[]>(`${API_CONFIG.baseUrl}/produtos/page?categorias=${categoria_id}`);
+   }
+
+   /**
+    *  Pega a imagem miniatura do bucket
+    *   @param id -> o identificador da imagem  
+    * **/
+   pegarImagemPequena(id: string): Observable<any> {
+      console.log("[produto Service] Identificador do produto " + id);
+      let url = `${API_CONFIG.bucketBaseURL}/prod${id}-small.jpg`;
+      console.log("[produto service] Url da imagem : " + url);
+      return this.http.get(url, { responseType: 'blob' });
    }
 }
